1bcc7144d80239c5edaec9f1aebc00bb6d4f03bb
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-11-01 09:44:13 -0400

    user test successfull

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const { connect, connection } = require('mongoose');
+//const mongoose = require('mongoose');
 require('dotenv').config();
 
-connect( process.env.MONGODB_TEMP, {
+connect( 'mongodb://127.0.0.1:27017/claimsDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
