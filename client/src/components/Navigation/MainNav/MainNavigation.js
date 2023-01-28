acfb43b5d5a63a5d2568cb98696ae9539e20df7c
Jasmine Daniels <create.jasminedaniels@gmail.com>
2023-01-28 12:49:04 -0500

    main nav css edit

--- client/src/components/Navigation/MainNav/MainNavigation.js
+++ client/src/components/Navigation/MainNav/MainNavigation.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Navbar, Nav, Col } from 'react-bootstrap';
 import lighthouse120px from '../../../images/lighthouse-120px.png';
 import './main-nav.css';
 export default function MainNavigation() {
@@ -7,29 +7,32 @@ export default function MainNavigation() {
     return (
         <>
             <Row>
-                <nav className="nav navbar navbar-expand-lg">
+                <nav className="nav navbar navbar-expand-md">
                     <div className='col'>
-                        <div className='row'>
-                            <div className='brand-align'>
-                                <div className='col-sm-2'>
-                                    <Link className="navbar-brand" to={'/'}>
-                                        <img src={lighthouse120px} alt=""></img>
-                                    </Link>
-                                </div>
-                                <div className='text-start my-auto'>
-                                    <Link to={'/'} className='site-title'>Claims.io </Link> <br />
-                                    <span> by LightHouse</span>
-                                </div>
+                        <div className='brand-align'>
+                            <div >
+                                <Link className="navbar-brand" to={'/'}>
+                                    <img src={lighthouse120px} alt="" className='light-margins'></img>
+                                </Link>
                             </div>
+
+                            <div >
+                                <Link className="navbar-brand" to={'/'}>
+                                    <span to={'/'} className='site-title site-title-LH pt-4'>Claims.io </span> 
+                                    <span className='site-title-LH'> by LightHouse</span>
+                                </Link>
+                            </div>
+
                         </div>
+
                     </div>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div className='col-md-8'>
-                            <div className='main-navlinks rounded-pill mx-auto'>
+                        <div className='col pb-5'>
+                            <div className='main-navlinks rounded-pill mx-auto fix-center '>
                                 <ul className='justify-content-center'>
                                     <Link to={'/about'} className="nav-link mx-2">About</Link>
                                     <Link to={'/pricing'} className="nav-link ">Pricing</Link>
@@ -39,13 +42,13 @@ export default function MainNavigation() {
                             </div>
                         </div>
 
-                        <div className='col'>
+                        <div className='col-md-'>
                             <div className='row'>
-                                <form className="form-inline my-2 my-lg-0 spaced-out">
-                                    <div className='col-md-7 mx-2'>
+                                <form className="form-inline my-2 my-lg-0 spaced-out ">
+                                    <div className='col-md-7 '>
                                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                                     </div>
-                                    <div className='col-md-4'>
+                                    <div className='col-md-4 mx-2'>
                                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                                     </div>
                                 </form>
