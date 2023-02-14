8a5150eccf534073c650c8ac25ee06dc2b25c395
Jasmine Daniels <create.jasminedaniels@gmail.com>
2023-02-14 14:53:10 -0500

    responsive css --wip

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,8 +10,8 @@ const Home = () => {
             <br></br>
             <h1>Welcome to Claims.io</h1>
 
-            <p>The Claims.io front-end is currently in development.</p>
-            <p >Feel Free to <Link to={'/about'} style={{color: "#fadd8f"}}>Explore</Link> and <Link to={'/user-signup'} style={{color: "#ffbd00"}}>Create a Free Account</Link> today to learn more about Claims.io!</p>
+            <p className='text-center'>The Claims.io front-end is currently in development.</p>
+            <p className='text-center'>Feel Free to <Link to={'/about'} style={{color: "#fadd8f"}}>Explore</Link> and <Link to={'/user-signup'} style={{color: "#ffbd00"}}>Create a Free Account</Link> today to learn more about Claims.io!</p>
 
 
             {/* <a
